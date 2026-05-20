62a1c00c3110d37d0bbf95122717da0e009a0642
lakshaysatija14-ux <lakshaysatija14@gmail.com>
2026-05-20 19:51:05 +0530

    LSM6DSL

---
+++ LSM6DSL_Accelerometer_SPI_Interface/Debug/Core/Src/lsm6dsl.d
@@ -0,0 +1,7 @@
+Core/Src/lsm6dsl.o: ../Core/Src/lsm6dsl.c \
+ D:/ide\ and\ mx/exp\ 6/Drivers/BSP/Components/lsm6dsl/lsm6dsl.h \
+ D:/ide\ and\ mx/exp\ 6/Drivers/BSP/Components/lsm6dsl/../Common/accelero.h \
+ D:/ide\ and\ mx/exp\ 6/Drivers/BSP/Components/lsm6dsl/../Common/gyro.h
+D:/ide\ and\ mx/exp\ 6/Drivers/BSP/Components/lsm6dsl/lsm6dsl.h:
+D:/ide\ and\ mx/exp\ 6/Drivers/BSP/Components/lsm6dsl/../Common/accelero.h:
+D:/ide\ and\ mx/exp\ 6/Drivers/BSP/Components/lsm6dsl/../Common/gyro.h:
---
+++ LSM6DSL_Accelerometer_SPI_Interface/Debug/Core/Src/subdir.mk
@@ -0,0 +1,54 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (13.3.rel1)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Core/Src/lsm6dsl.c \
+../Core/Src/main.c \
+../Core/Src/stm32l475e_iot01.c \
+../Core/Src/stm32l475e_iot01_accelero.c \
+../Core/Src/stm32l475e_iot01_gyro.c \
+../Core/Src/stm32l4xx_hal_msp.c \
+../Core/Src/stm32l4xx_it.c \
+../Core/Src/syscalls.c \
+../Core/Src/sysmem.c \
+../Core/Src/system_stm32l4xx.c 
+
+OBJS += \
+./Core/Src/lsm6dsl.o \
+./Core/Src/main.o \
+./Core/Src/stm32l475e_iot01.o \
+./Core/Src/stm32l475e_iot01_accelero.o \
+./Core/Src/stm32l475e_iot01_gyro.o \
+./Core/Src/stm32l4xx_hal_msp.o \
+./Core/Src/stm32l4xx_it.o \
+./Core/Src/syscalls.o \
+./Core/Src/sysmem.o \
+./Core/Src/system_stm32l4xx.o 
+
+C_DEPS += \
+./Core/Src/lsm6dsl.d \
+./Core/Src/main.d \
+./Core/Src/stm32l475e_iot01.d \
+./Core/Src/stm32l475e_iot01_accelero.d \
+./Core/Src/stm32l475e_iot01_gyro.d \
+./Core/Src/stm32l4xx_hal_msp.d \
+./Core/Src/stm32l4xx_it.d \
+./Core/Src/syscalls.d \
+./Core/Src/sysmem.d \
+./Core/Src/system_stm32l4xx.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Core/Src/%.o Core/Src/%.su Core/Src/%.cyclo: ../Core/Src/%.c Core/Src/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32L475xx -c -I"D:/ide and mx/exp 6/Drivers/BSP/B-L475E-IOT01" -I"D:/ide and mx/exp 6/Drivers/BSP/Components/lsm6dsl" -I../Core/Inc -I../Drivers/STM32L4xx_HAL_Driver/Inc -I../Drivers/STM32L4xx_HAL_Driver/Inc/Legacy -I../Drivers/CMSIS/Device/ST/STM32L4xx/Include -I../Drivers/CMSIS/Include -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+
+clean: clean-Core-2f-Src
+
+clean-Core-2f-Src:
+	-$(RM) ./Core/Src/lsm6dsl.cyclo ./Core/Src/lsm6dsl.d ./Core/Src/lsm6dsl.o ./Core/Src/lsm6dsl.su ./Core/Src/main.cyclo ./Core/Src/main.d ./Core/Src/main.o ./Core/Src/main.su ./Core/Src/stm32l475e_iot01.cyclo ./Core/Src/stm32l475e_iot01.d ./Core/Src/stm32l475e_iot01.o ./Core/Src/stm32l475e_iot01.su ./Core/Src/stm32l475e_iot01_accelero.cyclo ./Core/Src/stm32l475e_iot01_accelero.d ./Core/Src/stm32l475e_iot01_accelero.o ./Core/Src/stm32l475e_iot01_accelero.su ./Core/Src/stm32l475e_iot01_gyro.cyclo ./Core/Src/stm32l475e_iot01_gyro.d ./Core/Src/stm32l475e_iot01_gyro.o ./Core/Src/stm32l475e_iot01_gyro.su ./Core/Src/stm32l4xx_hal_msp.cyclo ./Core/Src/stm32l4xx_hal_msp.d ./Core/Src/stm32l4xx_hal_msp.o ./Core/Src/stm32l4xx_hal_msp.su ./Core/Src/stm32l4xx_it.cyclo ./Core/Src/stm32l4xx_it.d ./Core/Src/stm32l4xx_it.o ./Core/Src/stm32l4xx_it.su ./Core/Src/syscalls.cyclo ./Core/Src/syscalls.d ./Core/Src/syscalls.o ./Core/Src/syscalls.su ./Core/Src/sysmem.cyclo ./Core/Src/sysmem.d ./Core/Src/sysmem.o ./Core/Src/sysmem.su ./Core/Src/system_stm32l4xx.cyclo ./Core/Src/system_stm32l4xx.d ./Core/Src/system_stm32l4xx.o ./Core/Src/system_stm32l4xx.su
+
+.PHONY: clean-Core-2f-Src
+
