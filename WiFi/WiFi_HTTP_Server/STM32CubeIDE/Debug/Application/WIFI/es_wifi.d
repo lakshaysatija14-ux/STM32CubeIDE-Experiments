11aa7784a87da1df1ad6735101d5b03da0723e41
lakshaysatija14-ux <lakshaysatija14@gmail.com>
2026-05-20 20:03:37 +0530

    Wifi Client/Server

---
+++ WiFi/WiFi_HTTP_Server/STM32CubeIDE/Debug/Application/WIFI/es_wifi.d
@@ -0,0 +1,5 @@
+Application/WIFI/es_wifi.o: \
+ C:/Users/Admin/STM32Cube/Repository/STM32Cube_FW_L4_V1.18.2/Projects/B-L475E-IOT01A/Applications/WiFi/Common/Src/es_wifi.c \
+ ../../../Common/Inc/es_wifi.h ../../Inc/es_wifi_conf.h
+../../../Common/Inc/es_wifi.h:
+../../Inc/es_wifi_conf.h:
---
+++ WiFi/WiFi_HTTP_Server/STM32CubeIDE/Debug/Drivers/BSP/hts221.d
@@ -0,0 +1,8 @@
+Drivers/BSP/hts221.o: \
+ C:/Users/Admin/STM32Cube/Repository/STM32Cube_FW_L4_V1.18.2/Drivers/BSP/Components/hts221/hts221.c \
+ C:/Users/Admin/STM32Cube/Repository/STM32Cube_FW_L4_V1.18.2/Drivers/BSP/Components/hts221/hts221.h \
+ C:/Users/Admin/STM32Cube/Repository/STM32Cube_FW_L4_V1.18.2/Drivers/BSP/Components/hts221/../Common/hsensor.h \
+ C:/Users/Admin/STM32Cube/Repository/STM32Cube_FW_L4_V1.18.2/Drivers/BSP/Components/hts221/../Common/tsensor.h
+C:/Users/Admin/STM32Cube/Repository/STM32Cube_FW_L4_V1.18.2/Drivers/BSP/Components/hts221/hts221.h:
+C:/Users/Admin/STM32Cube/Repository/STM32Cube_FW_L4_V1.18.2/Drivers/BSP/Components/hts221/../Common/hsensor.h:
+C:/Users/Admin/STM32Cube/Repository/STM32Cube_FW_L4_V1.18.2/Drivers/BSP/Components/hts221/../Common/tsensor.h:
